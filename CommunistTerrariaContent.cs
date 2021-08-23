a143c1763916bcd446e070903626846e48acc6a6
MikhailMCraft <59270079+MikhailMCraft@users.noreply.github.com>
2021-08-23 12:11:36 -0400

    making sure Our music is unloaded

--- CommunistTerrariaContent.cs
+++ CommunistTerrariaContent.cs
@@ -1,5 +1,6 @@
 using Terraria;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Audio;
 
 namespace CommunistTerrariaContent
 {
@@ -9,6 +10,17 @@ namespace CommunistTerrariaContent
             if (!Main.dedServ) {
                 AddMusicBox(CommunistTerraria.CommunistTerraria.Mod.GetSoundSlot(SoundType.Music, "Sounds/Music/StateAnthemOfTheUSSR"), ModContent.ItemType<Items.OurMusicBox>(), ModContent.TileType<Tiles.OurMusicBox>());
             }
+        }
+
+        public override void Close() {
+            int soundSlot = CommunistTerraria.CommunistTerraria.Mod.GetSoundSlot(SoundType.Music, "Sounds/Music/StateAnthemOfTheUSSR");
+            if (Main.music.IndexInRange(soundSlot)) {
+                var check = Main.music[soundSlot];
+                if (check != null && check.IsPlaying) {
+                    Main.music[soundSlot].Stop(AudioStopOptions.Immediate);
+                }
+            }
+            base.Close();
         }
     }
 }
