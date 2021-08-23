d47ad5b37ba87d57a15b9ab9692c5a555f69bc50
MikhailMCraft <59270079+MikhailMCraft@users.noreply.github.com>
2021-08-23 12:00:31 -0400

    adding our music box!

--- CommunistTerrariaContent.cs
+++ CommunistTerrariaContent.cs
@@ -1,8 +1,14 @@
+using Terraria;
 using Terraria.ModLoader;
 
 namespace CommunistTerrariaContent
 {
 	public class CommunistTerrariaContent : Mod
 	{
-	}
+        public override void Load() {
+            if (!Main.dedServ) {
+                AddMusicBox(CommunistTerraria.CommunistTerraria.Mod.GetSoundSlot(SoundType.Music, "Sounds/Music/StateAnthemOfTheUSSR"), ModContent.ItemType<Items.OurMusicBox>(), ModContent.TileType<Tiles.OurMusicBox>());
+            }
+        }
+    }
 }
---
+++ Items/OurMusicBox.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CommunistTerrariaContent.Items
+{
+    public class OurMusicBox : ModItem
+    {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Music Box (Anthem)");
+        }
+
+		public override void SetDefaults() {
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.useTurn = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.autoReuse = true;
+			item.consumable = true;
+			item.createTile = ModContent.TileType<Tiles.OurMusicBox>();
+			item.width = 24;
+			item.height = 24;
+			item.rare = ItemRarityID.LightRed;
+			item.value = Item.buyPrice(gold: 2);
+			item.accessory = true;
+		}
+	}
+}
